4ff906afdec86387bed7bc8c8e3da203a593a491
zhenghongyu <1193073442@qq.com>
2021-10-12 13:48:39 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author zhy
+ * @create 2021-10-12 13:45
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+
+}
